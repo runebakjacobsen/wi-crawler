029a937ddd42c6d4f3870ecb8f0b7a40cded4ca3
Rune Bak Jacobsen <runebakjacobsen@gmail.com>
2019-12-27 15:47:22 +0100

    Add extracting links from visited webpage

--- Crawler.cs
+++ Crawler.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -6,7 +7,14 @@ namespace wi_crawler
 {
     public class Crawler
     {
-        public async Task<string> GetHTML(string url)
+        public Crawler(string seedUrl)
+        {
+            SetBaseUrl(seedUrl);
+        }
+
+
+        private string _baseUrl;
+        public string GetHTML(string url)
         {
             var httpClient = new HttpClient();
             var html = httpClient.GetStringAsync(url);
@@ -14,12 +22,90 @@ namespace wi_crawler
             return html.Result;
         }
 
-        public async Task<string> GetRobotsTxt(string url)
+        private void SetBaseUrl(string url)
+        {
+            Uri uri = new Uri(url);
+            _baseUrl = uri.Host;
+        }
+
+        public string GetContent(string html)
+        {
+            var htmlDocument = new HtmlDocument();
+
+            htmlDocument.LoadHtml(html);
+
+            return htmlDocument.ParsedText;
+            // TODO - escape html
+
+        }
+
+        public List<string> GetLinkTos(string html)
+        {
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+            var linkTos = new List<string>();
+
+            foreach (HtmlNode link in htmlDocument.DocumentNode.SelectNodes("//a[@href]"))
+            {
+                string hrefValue = link.GetAttributeValue("href", string.Empty);
+                // TODO Refactor this
+                if (!IsWebUrl(hrefValue))
+                {
+                    continue;
+                }
+                var normalizedUrl = NormalizeUrl(hrefValue);
+                linkTos.Add(normalizedUrl);
+            }
+
+            return linkTos;
+        }
+
+        private bool IsWebUrl(string url)
+        {
+            if (url.StartsWith("mailto:") || url.StartsWith("tel:"))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private string NormalizeUrl(string url)
+        {
+            if (url.StartsWith("//"))
+            {
+                url = "http:" + url;
+            }
+
+            if (!url.Contains("://"))
+            {
+                url = "http://" + _baseUrl + url;
+            }
+
+            return url;
+        }
+
+        public string GetRobotsTxt(string baseUrl)
         {
             var httpClient = new HttpClient();
-            var robots = httpClient.GetStringAsync($"{url}/robots.txt");
+            var robots = httpClient.GetStringAsync($"{baseUrl}/robots.txt");
 
             return robots.Result;
+        }
+
+        public async Task Frontier()
+        {
+            // TODO store list with already visited domains
+
+            // TODO Check if page passes near duplicate analysis, If it does not skip it, else add it 
+
+            // TODO Extract “link-to” URLs 
+            // Normalize URL
+            // b. Check that it passes certain URL filter tests. E.g.:
+            // • Focused crawl: only crawl .dk
+            // • Obey robots.txt (freshness caveat)
+            // c. Check that not already in frontier
         }
     }
 }
