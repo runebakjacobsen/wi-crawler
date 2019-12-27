f334f36ca85e969bc3e64106fa994d2d0c29d24c
Rune Bak Jacobsen <runebakjacobsen@gmail.com>
2019-12-27 11:39:37 +0100

    Add project files

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using System;
+
+namespace wi_crawler
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var crawler = new Crawler();
+            await crawler.GetRobotsTxt("http://dr.dk");
+        }
+    }
+}
