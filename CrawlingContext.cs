e462388ec3975b6a66a71c5846f831f2119f3505
Rune Bak Jacobsen <runebakjacobsen@gmail.com>
2019-12-27 15:46:29 +0100

    Add field to webpage model

--- CrawlingContext.cs
+++ CrawlingContext.cs
@@ -13,7 +13,8 @@ namespace wi_crawler
     public class Webpage
     {
         public int WebpageId { get; set; }
-        public string Html { get; set; }
+        public string HtmlContent { get; set; }
+        public string Content { get; set; }
         public string Url { get; set; }
         public string BaseDomain { get; set; }
 
