319ba05280a9285b0c4ccd2ca05dc6c1326a32e2
Rune Bak Jacobsen <runebakjacobsen@gmail.com>
2020-01-03 10:48:59 +0100

    WIP on transition probability matrix

---
+++ PageRanker.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Linq;
+
+namespace wi_crawler
+{
+    public class PageRanker
+    {
+        //* Just make it possible to get the pagerank for domain
+        //* Lets save it in the db or something? Then we can always implement it later.. 
+
+        double[,] transitionProbabilityMatrix;
+
+        public void buildMatrix()
+        {
+            using var db = new CrawlingContext();
+
+            var webpages = db.Webpages.ToList();
+
+            var matrix = new double[webpages.Count(), webpages.Count()];
+            int rowLength = matrix.GetLength(0);
+            int colLength = matrix.GetLength(1);
+            for (int i = 0; i < rowLength; i++)
+            {
+                var rowWebpage = webpages.Find(x => x.WebpageId == i + 1);
+                var rowUrl = new Uri(rowWebpage.Url);
+                for (int j = 0; j < colLength; j++)
+                {
+                    var frontier = new Frontier();
+                    var colWebpage = webpages.Find(x => x.WebpageId == j + 1);
+                    var outgoingLinks = frontier.GetLinkTos(colWebpage.HtmlContent).Select(x => new Uri(x)).ToList();
+                    double probability = 0;
+
+                    foreach (var link in outgoingLinks)
+                    {
+                        if (frontier.CompareUris(link, rowUrl) == 0)
+                        {
+                            probability = (double)1 / (double)outgoingLinks.Count();
+                        }
+                    }
+
+
+                    matrix[i, j] = probability;
+                }
+            }
+
+            for (int i = 0; i < rowLength; i++)
+            {
+                for (int j = 0; j < colLength; j++)
+                {
+                    Console.Write(string.Format("{0} ", matrix[i, j]));
+                }
+                Console.Write(Environment.NewLine + Environment.NewLine);
+            }
+            Console.ReadLine();
+        }
+    }
+}
